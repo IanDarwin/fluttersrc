1bd675a81ee63f413e0ef21fbbe2b09b424ed250
Ian Darwin <ian@darwinsys.com>
2023-01-31 20:15:05 +0300

    add fancy font from Google fonts

--- stack_rounded/lib/main.dart
+++ stack_rounded/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  // Obligatory license publication for Google Fonts
+  // LicenseRegistry.addLicense(() async* {
+  //   final license = await rootBundle.loadString('google_fonts/OFL.txt');
+  //   yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  // });
+
+  // Obligatory runApp call.
   runApp(const MyApp());
 }
 
@@ -30,11 +38,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  static const borderRadiusTop = BorderRadius.only(
+    topLeft: Radius.circular(32.0), topRight: Radius.circular(32.0));
+  static const borderRadiusDropLeft = BorderRadius.only(
+    topLeft: Radius.circular(32.0), bottomRight: Radius.circular(32.0));
+  static const borderRadiusDropRight = BorderRadius.only(
+    topRight: Radius.circular(32.0), bottomLeft: Radius.circular(32.0));
+
+  void doSomethingAndRedisplay() {
     setState(() {
-      _counter++;
+      //
     });
   }
 
@@ -47,51 +61,60 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Stack(
         children: <Widget>[
+          // Some text above the avatar
           Container(
             height: mq.size.height * 1.0/5.0,
             width: mq.size.width,
             decoration: const BoxDecoration(
               color:Color(0xffffffff),
             ),
-            child:Center(child:
-            Text("You snooz ya lose!",
+            child:const Center(child:
+            Text("You snooze you lose!",
                 style: TextStyle(
                     fontSize: 18,
                     color: Colors.black))),
           ),
 
+	        // The large blue box with rounded top
           Container(
-            margin:EdgeInsets.fromLTRB(0, 150, 0, 0),
-            padding:EdgeInsets.all(0),
+            margin: const EdgeInsets.fromLTRB(0, 150, 0, 0),
+            padding: const EdgeInsets.all(0),
             height:mq.size.height,
             width: mq.size.width,
             decoration: BoxDecoration(
-              color:Color(0xff0000ff),
+              color:const Color(0xff0000ff),
               shape:BoxShape.rectangle,
-              borderRadius:BorderRadius.only(topLeft: Radius.circular(32.0), topRight: Radius.circular(32.0)),
+              borderRadius:borderRadiusTop,
               border:Border.all(color:Color(0x4d9e9e9e),width:1),
             ),
             child: Center(child:
             Text("Your name in lights!",
-                style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white))),
+              style: GoogleFonts.explora(
+              textStyle: Theme.of(context).textTheme.headline4,
+              fontSize: 48,
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.italic,
+              color: Colors.white))
+            ),
           ),
+
+	        // The avatar
           Container(
-            margin:EdgeInsets.fromLTRB(0, 150-50, 0, 0),
+            margin: const EdgeInsets.fromLTRB(0, 150-50, 0, 0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children:[
+              children:const [
                 CircleAvatar(
                   radius: 50,
-                  backgroundImage: NetworkImage('https://www.gravatar.com/avatar/093d7a59c573047572c112bf79c83066'),
+                  backgroundImage: NetworkImage(
+		              'https://www.gravatar.com/avatar/093d7a59c573047572c112bf79c83066'),
                 )],
             ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => print("FAB Pressed"),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
