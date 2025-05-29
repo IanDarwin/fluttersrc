2d1a337e4646f6dc79d638fa9f2e11cdcb7eee82
Ian Darwin <ian@darwinsys.com>
2025-05-29 10:43:46 -0400

    Tweak formatting

--- flowcase/lib/path_provider_demo.dart
+++ flowcase/lib/path_provider_demo.dart
@@ -44,29 +44,32 @@ class PathProviderDemoState extends State<PathProviderDemo> {
     return ListTile(
       onTap: () => alert(context, explanation, title: "Details"),
       title: Text("$name:"),
+      titleTextStyle: const TextStyle(fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+          backgroundColor: Colors.white54,
+          color: Colors.black),
       subtitle: Text("$value"),
-      tileColor: notSupported == value ? Colors.red :
-        list.length % 2 == 0 ? Colors.orangeAccent : Colors.greenAccent,
+      tileColor: notSupported == value ? Colors.red : Colors.greenAccent,
     );
   }
 
   Future<bool> getData(BuildContext context, List list) async {
     list.add(_makeEntry("applicationCacheDirectory", await getApplicationCacheDirectory(),
-        "directory where the application may place application-specific cache files."));
+        "Directory where the application may place application-specific cache files."));
     list.add(_makeEntry("applicationDocumentsDirectory", await getApplicationDocumentsDirectory(),
-        "directory where the application may place data that is user-generated, or that cannot otherwise be recreated by your application."));
+        "Directory where the application may place data that is user-generated, or that cannot otherwise be recreated by your application."));
     list.add(_makeEntry("applicationSupportDirectory", await getApplicationSupportDirectory(),
-        "directory where the application may place application support files."));
+        "Directory where the application may place application support files."));
     list.add(_makeEntry("downloadsDirectory", await getDownloadsDirectory()??notSupported,
-        "directory where downloaded files can be stored."));
+        "Directory where downloaded files can be stored."));
     list.add(_makeEntry("externalCacheDirectories", await getExternalCacheDirectories()??notSupported,
-      "directories where application specific cache data can be stored externally."));
+      "Directories where application specific cache data can be stored externally."));
     list.add(_makeEntry("externalStorageDirectory", await getExternalStorageDirectory()??notSupported,
-        "directory where the application may access top level storage."));
+        "Directory where the application may access top level storage."));
     list.add(_makeEntry("libraryDirectory", Platform.isAndroid?notSupported:await getLibraryDirectory(),
-        "directory where application can store files that are persistent, backed up, and not visible to the user, such as sqlite.db."));
+        "Directory where application can store files that are persistent, backed up, and not visible to the user, such as sqlite.db."));
     list.add(_makeEntry("temporaryDirectory", await getTemporaryDirectory(),
-        "temporary directory on the device that is not backed up and is suitable for storing caches of downloaded files."));
+        "Temporary directory that is not backed up and is suitable for storing caches of downloaded files."));
     return true;
   }
 }
